5960c9d6bee95000aab588b81c4d228444cb70f8
sufiyan440 <76608895+sufiyan440@users.noreply.github.com>
2021-01-06 12:26:06 +0530

    Update App.java

--- src/main/java/javapro1/App.java
+++ src/main/java/javapro1/App.java
@@ -2,25 +2,16 @@ package javapro1;
 
 public class App {
 
-    public static void main(String[] args) {
-
-        float first = 1.20f, second = 2.45f;
-
-        System.out.println("--Before swap--");
-        System.out.println("First number = " + first);
-        System.out.println("Second number = " + second);
-
-        // Value of first is assigned to temporary
-        float temporary = first;
-
-        // Value of second is assigned to first
-        first = second;
-
-        // Value of temporary (which contains the initial value of first) is assigned to second
-        second = temporary;
-
-        System.out.println("--After swap--");
-        System.out.println("First number = " + first);
-        System.out.println("Second number = " + second);
-    }
+    public static void main(String[] args) {  
+        //Initialize array  
+        int [] arr = new int [] {1, 2, 3, 4, 2, 7, 8, 8, 3};  
+        System.out.println("Duplicate elements in given array: ");  
+        //Searches for duplicate element  
+        for(int i = 0; i < arr.length; i++) {  
+            for(int j = i + 1; j < arr.length; j++) {  
+                if(arr[i] == arr[j])  
+                    System.out.println(arr[j]);  
+            }  
+        }  
+    }  
 }
